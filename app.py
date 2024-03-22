f1c78ab30959c38ea53d0335c4d287b91fdd2f7b
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-03-22 10:59:37 +0000

    added letters guessed board

--- app.py
+++ app.py
@@ -2,9 +2,43 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+#code to run main game
+
+databaseWord = "testword"
+guessesLeft = 7
+eachGuessedLetter = ""
+correctLetters = ""
+
+while guessesLeft != 0:
+    guess = "hi"
+
+    if guess in databaseWord:
+        print("Correct")
+    else:
+        guessesLeft -= 1
+        print("wrong")
+
+    #storing guessed letters
+    eachGuessedLetter = eachGuessedLetter + guess
+    correctLetters = ""
+    for letter in databaseWord:
+        if letter in eachGuessedLetter:
+            correctLetters += f"{letter}"
+            correctLetters += " "
+        else:
+             correctLetters += "_"
+             correctLetters += " "
+
+
+
+
 @app.route('/')
 def index():
-    return render_template('gameboard.html')
+    return render_template('/index.html')
+@app.route('/gameboard')
+def gameboard():
+    return render_template('/gameboard.html', displayedLetters = correctLetters)
 
 if __name__ == "__main__":
     app.run(debug=True)
