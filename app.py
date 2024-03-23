3e111902ce18af755ae30eb3a0f271c72154fe23
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-03-23 12:15:10 +0000

    added buttons removing on click

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request,  request, render_template_string, jsonify
+from flask import Flask, render_template, request,  request, render_template_string, jsonify, session
 import time
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  
 
 
 #code to run main game
@@ -49,29 +50,40 @@ def hangman(guess):
 def index():
     return render_template('/index.html')
 
+# updates the buttons to be removed when clicked
+@app.route('/updateButtons')
+def updateButtons():
+    global eachGuessedLetter
+    buttonsToUpdate = eachGuessedLetter
+    print(buttonsToUpdate)
+    return jsonify(buttonsToUpdate)
+
+
 # used to grab a value from a button press to be used within the game
 @app.route('/button', methods=["GET", "POST"])
 def buttonpress():
+    
     data = request.json
     value = data['value']
     hangman(value)
-    print(f'Button clicked with value: {value}')
+
     return {'status': 'success', 'value_received': value}
 
 @app.route('/gameboard')
 def gameboard():
     time.sleep(0.1)
-    return render_template('/gameboard.html', displayedLetters = correctLetters)
+    return render_template('/gameboard.html', displayedLetters = correctLetters,)
+
 
 # updates the image bases on how many guesses are left
 @app.route('/update-image', methods=["GET", "POST"])
 def update_image():
     global guessesLeft
-    if guessesLeft is 7:
+    if guessesLeft == 7:
         new_image_src = "/static/images/hang1.png" 
-    elif guessesLeft is 6:
+    elif guessesLeft == 6:
         new_image_src = "/static/images/hang2.png"
-    elif guessesLeft is 5:
+    elif guessesLeft == 5:
         new_image_src = "/static/images/hang3.png"
     elif guessesLeft == 4:
         new_image_src = "/static/images/hang4.png"
