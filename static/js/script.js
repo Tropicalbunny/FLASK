0fa9ef430f9ec95ba79a4864b6a353343595f4fa
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-03-23 14:45:49 +0000

    moved the js to seperate file as getting large

---
+++ static/js/script.js
@@ -0,0 +1,61 @@
+
+let infoOpen = document.getElementById('info-open');
+let infoContainer = document.getElementById('info-container');
+let infoClose = document.getElementById('info-close');
+
+function sendValue(buttonValue) {
+    fetch('/button', {
+        method: 'POST',
+        body: JSON.stringify({value: buttonValue}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+fetch('/update-image')
+    .then(response => response.json())
+    .then(data => {
+        if (data == 6) {
+            document.getElementById('hangman-image-inner').src = "/static/images/hang2.png"
+    } else if (data == 5){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang3.png"
+    } else if (data == 4){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang4.png"
+    } else if (data == 3){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang5.png"
+    } else if (data == 2){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang6.png"
+    } else if (data == 1){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang7.png"
+    } else if (data == 0){
+        document.getElementById('hangman-image-inner').src = "/static/images/hang8.png"
+    } else {
+        document.getElementById('hangman-image-inner').src = "/static/images/hang1.png" 
+    }
+});
+function hideButton(buttonId) {
+        var button = document.getElementById(buttonId);
+        button.style.display = 'none'; 
+    }
+fetch('/updateButtons')
+    .then(response => response.json())
+    .then(data => {
+    
+    let buttonsArray = data.split('');
+
+    buttonsArray.forEach(element => {
+        hideButton(element);
+        console.log(element);
+    });
+});
+
+infoOpen.addEventListener('click', function(){
+infoContainer.style.display = 'block';
+})
+
+infoClose.addEventListener('click', function(){
+    infoContainer.style.display = 'none';
+})
+
