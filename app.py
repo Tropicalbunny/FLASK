4b9c6d1ec26a1c2299b27db3f62b6aad3013c09d
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-04-16 11:31:42 +0100

    updated to external database

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, render_template_string, jsonify, redirect, url_for, flash
 import time
-import sqlite3
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)  
 app.secret_key = 'test'
-DB_NAME = "database.db"
-
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "loginInfo"
+COLLECTION = "Users"
 #code to run main game
 
 databaseWord = "testwordc"
@@ -14,6 +18,25 @@ eachGuessedLetter = ""
 correctLetters = ""
 value = ""
 gameOver = 0
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("error") %e
+
+
+conn = mongo_connect(MONGO_URI)
+
+
+coll = conn[DATABASE][COLLECTION]
+
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
 
 
 for letter in databaseWord:
@@ -61,6 +84,7 @@ def hangman(guess):
 def index():
     return render_template('index.html')
 
+
 # updates the buttons to be removed when clicked
 @app.route('/updateButtons')
 def updateButtons():
@@ -77,6 +101,7 @@ def buttonpress():
     hangman(value)
     return {'status': 'success', 'value_received': value}
 
+
 # used to create the connection for the gameboard html
 @app.route('/gameboard')
 def gameboard():
@@ -92,10 +117,12 @@ def update_image():
     return jsonify(imageUpdate)
 # used to tell java if the game is running, passed or failed
 
+
 @app.route('/isGameOver', methods=["GET", "POST"])
 def game_over():
     global gameOver
     return jsonify(gameOver)
+
 
 # used to tell the user they have won
 @app.route('/pass')
@@ -103,11 +130,13 @@ def passLevel():
     time.sleep(0.1)
     return render_template('pass.html',)
 
+
 # used to tell the user they have lost
 @app.route('/fail')
 def failLevel():
     time.sleep(0.1)
     return render_template('fail.html',)
+
 
 # used to login to the game
 @app.route('/login', methods=["GET", "POST"])
@@ -117,32 +146,32 @@ def login():
     print(data)
     return render_template('login.html',)
 
+
 # used to create a login for the game 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
     time.sleep(0.1)
     if request.method == 'POST':
-        email = request.form.get('email')
+        username = request.form.get('username')
         name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        query = "SELECT * FROM loginInfo WHERE email = ? "
-        conn = sqlite3.connect('loginInfo.db') 
-        cursor = conn.cursor() 
-        cursor.execute(query, (email,))
-        emailCheck = cursor.fetchone()
-
+        usernameCheck = coll.find({"username": username,})
         if password1 != password2:
             flash('Passwords do not match', category='fail')
         elif len(password1) < 5:
             flash('Password must be longer than 5 characters', category='fail')
-        elif emailCheck is not None:
-            flash('Email exists in the database', category='fail')
+        elif usernameCheck is not None:
+            flash('Username exists in the database', category='fail')
+            print(usernameCheck)
         else:
             flash('Account created!', category='pass')
-            cursor.execute("INSERT INTO loginInfo (name, email, password) VALUES (?, ?, ?)", ((name,), (email,), (password1,)))
-            conn.commit() 
-            conn.close() 
+            newInfo = {
+                    "name": name,
+                    "username": username,
+                    "password": password1 
+            }
+            coll.insert(newInfo)
             return render_template('index.html')
 
 
