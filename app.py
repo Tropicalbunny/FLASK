11ad51a30f13b593b45d6aa9853cf94747a5aac9
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-05-21 20:11:08 +0100

    set debug to false

--- app.py
+++ app.py
@@ -329,4 +329,4 @@ def addtitle():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
