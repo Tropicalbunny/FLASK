acdc48d61c795005f257b6d7a1c38e694711097d
Tropicalbunny <132086565+Tropicalbunny@users.noreply.github.com>
2024-05-21 23:13:24 +0100

    heroku deployment

--- app.py
+++ app.py
@@ -312,4 +312,6 @@ def addtitle():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
